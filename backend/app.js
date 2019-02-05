5c3d958ca1fa34e2ecacfd00749e5eeb1d9ed3cb
ferencDobi <ferencdbi@gmail.com>
2019-02-05 13:09:21 +0100

    add db connection

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const logger = require('log4js').getLogger('app');
+const mysql = require('mysql');
 
-const { PORT, NODE_ENV } = process.env;
+const connection = mysql.createConnection(require('./config/dbsettings'));
+
+connection.query('SELECT * FROM MenuItems;', (error, rows) => {
+  if (error) {
+    logger.error(error.message);
+  } else {
+    logger.warn(rows[0]);
+  }
+});
+
+connection.end();
+
+const port = process.env.PORT || 3001;
+const { NODE_ENV } = process.env;
 
 logger.level = NODE_ENV === 'development' ? 'info' : 'off';
 
@@ -15,6 +29,6 @@ app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(PORT, () => {
-  logger.info(`Listening on port ${PORT}...`);
+app.listen(port, () => {
+  logger.info(`Listening on port ${port}...`);
 });
