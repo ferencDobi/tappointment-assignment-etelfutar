c55632853e9064cae096c8db98e3cb1265fccab9
ferencDobi <ferencdbi@gmail.com>
2019-02-09 20:14:48 +0100

    resolve overflowing cart issue

--- frontend/src/components/container/Cart.jsx
+++ frontend/src/components/container/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { connect } from 'react-redux';
 import { Note } from '../presentational/Note';
 import { CartItem } from '../presentational/CartItem';
@@ -6,14 +6,20 @@ import { removeFromCart } from '../../actions/cartActions';
 
 const Cart = ({ cart, total, removeFromCart }) => {
   return (
-    <Note title="Rendelés">
-      {cart.map((item, i) => 
-        <CartItem key={'item-' + i} className="text-line"
-                  item={item} action={removeFromCart} />)}
-      <div className="text-line">
-        <p>{total} Ft</p>
-      </div>
-    </Note>
+    <Fragment>
+      <Note title="Rendelés">
+        {cart.slice(0, 10).map((item, i) => 
+          <CartItem key={'item-' + i} className="text-line"
+                    item={item} action={removeFromCart} />)}
+        {cart.length <= 10 && <div className="text-line end"><p>{total} Ft</p></div>}
+      </Note>
+      {cart.length > 10 && <Note className="second">
+        {cart.slice(10).map((item, i) =>
+          <CartItem key={'item-' + i + 10} className="text-line"
+                    item={item} action={removeFromCart} />)}
+          <div className="text-line end"><p>{total} Ft</p></div>
+      </Note>}
+    </Fragment>
   );
 }
 
--- frontend/src/components/presentational/Note.jsx
+++ frontend/src/components/presentational/Note.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const Note = ({ title, children }) => (
-  <div class="paper">
-    <div class="lines">
+export const Note = ({ title, children, className = '' }) => (
+  <div className={className + ' paper'}>
+    <div className="lines">
       <img src="http://www.pngmart.com/files/3/Pushpin-PNG-Photos.png" alt="" />
-      <h3 class="title">{title}</h3>
-      <div class="text">{children}</div>
+      <h3 className="title">{title}</h3>
+      <div className="text">{children}</div>
     </div>
   </div>
 );
